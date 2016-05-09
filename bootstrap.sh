f090ac7a9c770b78c55db13658ed0519bc9adbde
vfarcic <viktor@farcic.com>
2016-05-09 22:04:07 +0200

    Updated Ansible, Docker, and Jenkins versions

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 
 echo "Installing Ansible..."
-#apt-get install -y software-properties-common
-#apt-add-repository ppa:ansible/ansible
-#apt-get update
-#apt-get install -y ansible
-apt-get install -y python-pip python-dev
-pip install ansible==1.9.2
-mkdir /etc/ansible
-touch /etc/ansible/hosts
+apt-get install -y software-properties-common
+apt-add-repository ppa:ansible/ansible
+apt-get update
+apt-get install -y ansible
+cp /vagrant/ansible/ansible.cfg /etc/ansible/ansible.cfg
